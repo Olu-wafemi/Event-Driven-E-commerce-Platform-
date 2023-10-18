ec8e741bb88193283f7ffeae9dbe2016f91fe34a
Oluwafemi <femio82@gmail.com>
2023-10-18 13:14:36 +0100

    added:Payment_model

---
+++ src/models/Payment.ts
@@ -0,0 +1,47 @@
+import { User } from './User';
+import Order from './Order';
+import {
+    Table,
+    Column,
+    DataType,
+    PrimaryKey,
+    ForeignKey,
+    HasMany,
+    BelongsTo,
+    Default,
+    Model
+
+} from 'sequelize-typescript'
+
+
+@Table({
+    tableName: 'payment'
+
+})
+
+class Payment extends Model{
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column(DataType.UUID)
+    id?: string;
+
+    @ForeignKey(()=> Order)
+    @Column(DataType.STRING)
+    userId?:string
+
+    @BelongsTo(()=> Order)
+    user?: Order
+
+    @Column(DataType.STRING)
+    paymentMethod?: string
+
+    @Column(DataType.DECIMAL(10,2))
+    amounnt?: number;
+
+    @Column(DataType.DATE)
+    paymentDate?: Date
+
+
+}
+
+export default Payment
