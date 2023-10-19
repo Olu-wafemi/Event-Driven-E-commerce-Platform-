410182db3e0155cae47385bb1efdfd7aa6680603
Oluwafemi <femio82@gmail.com>
2023-10-19 11:27:59 +0100

    added:vendor_model

---
+++ src/models/Vendor.ts
@@ -0,0 +1,42 @@
+import {
+    Table,
+    Column,
+    Default,
+    Model,
+    PrimaryKey,
+    ForeignKey,
+    HasMany,
+    BelongsTo,
+    DataType,
+    Unique,
+
+
+} from 'sequelize-typescript'
+
+@Table({
+    tableName: 'vendor'
+})
+class Vendor extends Model{
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column(DataType.UUID)
+    id?: string
+
+    @Unique
+    @Column(DataType.STRING)
+    name?: string
+
+    @Column(DataType.STRING)
+    description?: string
+
+    @Column(DataType.STRING)
+    contactEmail?: string
+
+    @Column(DataType.NUMBER)
+    phone?: number
+
+    @Column(DataType.STRING)
+    website?: string
+}
+
+export default Vendor
