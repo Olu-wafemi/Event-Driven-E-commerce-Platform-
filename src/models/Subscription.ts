2ef22d89101f6788fab7af30866a15ab1118a7fb
Oluwafemi <femio82@gmail.com>
2023-10-19 11:33:46 +0100

    added:Subscription_model

---
+++ src/models/Subscription.ts
@@ -0,0 +1,34 @@
+import { BelongsTo, Column, DataType, Default, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { User } from './User';
+
+
+@Table({
+    tableName: 'subscriptions'
+})
+
+class Subscription extends Model{
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column(DataType.UUID)
+    id?: string
+
+    @ForeignKey(()=> User)
+    @Column(DataType.STRING)
+    userId?: string
+
+    @BelongsTo(()=> User)
+    user?: User
+
+    @Column(DataType.STRING)
+    planName?: string
+
+    @Column(DataType.DECIMAL(10,2))
+    price?: number
+
+    @Column(DataType.DATE)
+    startDate?: Date
+
+    @Column(DataType.DATE)
+    endDate?: Date
+}
+export default Subscription
