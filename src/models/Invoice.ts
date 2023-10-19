68e35d74c4a13855226d5c1c861e9ec9eee2d372
Oluwafemi <femio82@gmail.com>
2023-10-19 11:22:00 +0100

    added:invoice_model

---
+++ src/models/Invoice.ts
@@ -0,0 +1,49 @@
+import { User } from './User';
+import Order from './Order';
+import Payment from './Payment';
+import {
+
+    Table,
+    Column,
+    Default,
+    HasMany,
+    BelongsTo,
+    HasOne,
+    PrimaryKey,
+    ForeignKey,
+    Model,
+    DataType
+    
+} from 'sequelize-typescript'
+
+@Table({
+    tableName: 'invoice'
+})
+class Invoice extends Model{
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column(DataType.UUID)
+    id?: string
+
+    @ForeignKey(()=> Order)
+    @Column(DataType.STRING)
+    orderId?: string
+
+
+    @BelongsTo(()=> Order)
+    order?: Order
+
+
+    @Column(DataType.DATE)
+    invoiceDate?: Date
+
+    @Column(DataType.DECIMAL(10,2))
+    totalAmount?: number
+
+    @Column(DataType.STRING)
+    paymentStatus?: string
+
+    
+}
+
+export default Invoice
