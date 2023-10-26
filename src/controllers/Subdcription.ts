b3244aa5a11bf572c25523f000b5e5b84e62e5e0
Oluwafemi <femio82@gmail.com>
2023-10-26 17:42:00 +0100

    added:suscription_controller

---
+++ src/controllers/Subdcription.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import Subscription from '../models/Subscription';
+export const SubscriptionController ={
+
+    async createSubscription(req: Request, res: Response){
+
+        try{
+            const { userId, planId, startDate, endDate} = req.body
+
+            const newSubscription = await Subscription.create({
+                userId,
+                planId,
+                startDate,
+                endDate
+            })
+
+            res.status(201).json(newSubscription)
+
+        }
+        catch{
+
+            res.status(500).json({error: "Error creating Subscription"})
+
+        }
+      
+
+    },
+    async getUserSubscriptiond(req: Request, res: Response){
+
+        try{
+            const {userId} = req.params;
+
+            const subscriptions = await Subscription.findAll({
+                where:{userId}
+            })
+
+            res.status(201).json({status:"true", data: subscriptions})
+        }
+        catch{
+            res.status(500).json({error: "Error fectching subscriptions"})
+        }
+    }
+}
