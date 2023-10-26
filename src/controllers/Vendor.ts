f2d0a68ea13cbec94fa7173e8622727d4a34098f
Oluwafemi <femio82@gmail.com>
2023-10-26 17:34:46 +0100

    added:vendor_controller

---
+++ src/controllers/Vendor.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import Vendor from '../models/Vendor';
+
+export const VendorContoller = {
+    async createVendor(req: Request, res:Response){
+
+        try{
+            const {name, description, email,phone} = req.body
+
+            const newVendor = await Vendor.create({
+                name, description,
+                email,phone
+            })
+
+            res.status(201).json({status: "true", data: newVendor})
+
+    }
+    catch{
+        res.status(500).json({error: "error creating vendor"})
+
+    }
+},
+async getVendors(req: Request, res: Response){
+    try{
+        const vendors = await Vendor.findAll()
+        res.status(201).json({status:"true", data:  vendors})
+
+
+    }
+    catch{
+        res.status(500).json({error: 'Error fecthing vendors'})
+    }
+}
+
+
+}
