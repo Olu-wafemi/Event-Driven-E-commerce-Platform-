4e129ad26cf685f4c5991bf2b96ea1d65fa41e2e
Oluwafemi <femio82@gmail.com>
2023-10-26 17:17:56 +0100

    added:coupon_controller

---
+++ src/controllers/Coupon.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import Coupon from '../models/Coupon';
+
+
+export const CouponController = {
+    async createCoupon(req: Request, res: Response){
+
+        try{
+            const {code, discountPercentage, validUntil} = req.body;
+
+            const newCoupon = await Coupon.create({
+                code, discountPercentage,
+                validUntil
+            })
+
+            res.status(201).json(newCoupon)
+        }
+        catch(error){
+
+
+        }
+        res.status(500).json({error : "error creating coupon"})
+
+},
+
+ async getCoupons(req:Request, res: Response){
+
+    try{
+        const coupons =await Coupon.findAll()
+        res.status(201).json({status: true, data:coupons})
+    }
+    catch{
+        res.status(500).json({error: 'Error fecthing coupons'})
+
+        
+    }
+ }
+}
