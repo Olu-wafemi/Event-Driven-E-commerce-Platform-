04c8aec9a4cf96ee23b5f4332f2fd38d86b5629c
Oluwafemi <femio82@gmail.com>
2023-10-30 23:35:48 +0100

    added:order_test

---
+++ test/order.controller.ts
@@ -0,0 +1,24 @@
+// order.controller.test.ts
+import request from 'supertest';
+import app from '../index'; // Import your Express app
+
+describe('Order Controller', () => {
+  it('should create a new order', async () => {
+    const newOrder = { /* Define the order data */ };
+    const response = await request(app)
+      .post('/api/orders')
+      .send(newOrder);
+
+    expect(response.status).toBe(201);
+    expect(response.body /* Check the response body */);
+  });
+
+  it('should retrieve a list of orders', async () => {
+    const response = await request(app).get('/api/orders');
+
+    expect(response.status).toBe(200);
+    expect(response.body /* Check the response body */);
+  });
+
+  // Add more test cases for other controller methods
+});
